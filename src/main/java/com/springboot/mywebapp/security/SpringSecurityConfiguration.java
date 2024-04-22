d0e7f2011fddb28f6e784a52d6d9c418ab75382d
Eleri Mets <eleri.apsolon@gmail.com>
2024-04-22 15:50:48 +0300

    Set up new user.

--- src/main/java/com/springboot/mywebapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/springboot/mywebapp/security/SpringSecurityConfiguration.java
@@ -17,15 +17,23 @@ public class SpringSecurityConfiguration {
 
     @Bean
     public  InMemoryUserDetailsManager createDetailsManager() {
+
+        UserDetails userDetails1 = createNewUser("Eleri", "1234");
+        UserDetails userDetails2 = createNewUser("Test", "dummy");
+
+        return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+    }
+
+    private UserDetails createNewUser(String username, String password) {
         Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
+
         UserDetails userDetails = User.builder()
                 .passwordEncoder(passwordEncoder)
-                .username("Eleri")
-                .password("1234")
+                .username(username)
+                .password(password)
                 .roles("USER", "ADMIN")
                 .build();
-
-        return new InMemoryUserDetailsManager(userDetails);
+        return userDetails;
     }
 
     @Bean
