869e3b24fef77b99cf5fa5607262d7006f5fd213
Eleri Mets <eleri.apsolon@gmail.com>
2024-04-22 20:24:29 +0300

    Annotate Todo with @Entity and create data.sql file with dummy data.

--- src/main/java/com/springboot/mywebapp/todo/Todo.java
+++ src/main/java/com/springboot/mywebapp/todo/Todo.java
@@ -1,11 +1,20 @@
 package com.springboot.mywebapp.todo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
 
 import java.time.LocalDate;
 
+@Getter
+@Entity
 public class Todo {
 
+    @Id
+    @GeneratedValue
     private int id;
     private String username;
 
@@ -23,40 +32,20 @@ public class Todo {
         this.done = done;
     }
 
-    public int getId() {
-        return id;
-    }
-
     public void setId(int id) {
         this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
     }
 
     public void setUsername(String username) {
         this.username = username;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
     public void setDescription(String description) {
         this.description = description;
     }
 
-    public LocalDate getTargetDate() {
-        return targetDate;
-    }
-
     public void setTargetDate(LocalDate targetDate) {
         this.targetDate = targetDate;
-    }
-
-    public boolean isDone() {
-        return done;
     }
 
     public void setDone(boolean done) {
